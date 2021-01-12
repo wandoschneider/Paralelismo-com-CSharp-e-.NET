614fd13cf4b729348e39a123f35f984ad4403866
Wando Schneider <wandoschneider@MacBook-Air-de-Wando.local>
2021-01-11 22:21:19 -0300

    alteraçoes utilizando async await

--- ByteBank.View/MainWindow.xaml.cs
+++ ByteBank.View/MainWindow.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using ByteBank.Core.Model;
 using ByteBank.Core.Repository;
 using ByteBank.Core.Service;
-using ByteBank.View.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,7 +23,6 @@ namespace ByteBank.View
     {
         private readonly ContaClienteRepository r_Repositorio;
         private readonly ContaClienteService r_Servico;
-        private CancellationTokenSource _cts;
 
         public MainWindow()
         {
@@ -38,70 +36,26 @@ namespace ByteBank.View
         {
             BtnProcessar.IsEnabled = false;
 
-            _cts = new CancellationTokenSource();
-
             var contas = r_Repositorio.GetContaClientes();
-
-            PgsProgresso.Maximum = contas.Count();
-
-            LimparView();
+           
+            AtualizarView(new List<string>(), TimeSpan.Zero);
 
             var inicio = DateTime.Now;
 
-            BtnCancelar.IsEnabled = true;
-            var progress = new Progress<String>(str =>
-                PgsProgresso.Value++);
-            //var byteBankProgress = new ByteBankProgress<String>(str =>
-            //  PgsProgresso.Value++);
+            var resultado = await ConsolidarContas(contas);
 
-            try
-            {
-                var resultado = await ConsolidarContas(contas, progress, _cts.Token);
-
-                var fim = DateTime.Now;
-                AtualizarView(resultado, fim - inicio);
-            }
-            catch (OperationCanceledException)
-            {
-                TxtTempo.Text = "Operação cancelada pelo usuário";
-            } 
-            finally
-            {
-                BtnProcessar.IsEnabled = true;
-                BtnCancelar.IsEnabled = false;
-            }
+            var fim = DateTime.Now;
+            AtualizarView(resultado, fim - inicio);
+            BtnProcessar.IsEnabled = true;
         }
 
-        private void BtnCancelar_Click(object sender, RoutedEventArgs e)
-        {
-            BtnCancelar.IsEnabled = false;
-            _cts.Cancel();
-        }
-
-        private async Task<string[]> ConsolidarContas(IEnumerable<ContaCliente> contas, IProgress<string> reportadorDeProgresso, CancellationToken ct)
+        private async Task<string[]> ConsolidarContas(IEnumerable<ContaCliente> contas)
         {
             var tasks = contas.Select(conta =>
-                Task.Factory.StartNew(() =>
-                {
-                    ct.ThrowIfCancellationRequested();
-
-                    var resultadoConsolidacao = r_Servico.ConsolidarMovimentacao(conta, ct);
-
-                    reportadorDeProgresso.Report(resultadoConsolidacao);
-
-                    ct.ThrowIfCancellationRequested();
-                    return resultadoConsolidacao;
-                }, ct)
+                Task.Factory.StartNew(() => r_Servico.ConsolidarMovimentacao(conta))
             );
 
             return await Task.WhenAll(tasks);
-        }
-
-        private void LimparView()
-        {
-            LstResultados.ItemsSource = null;
-            TxtTempo.Text = null;
-            PgsProgresso.Value = 0;
         }
 
         private void AtualizarView(IEnumerable<String> result, TimeSpan elapsedTime)
